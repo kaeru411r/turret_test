be8297583693aa42a6b3c2b295e70492f909df69
kaeru411r <ike269fr@gmail.com>
2022-12-25 21:53:01 +0900

    関数内で変更しない引数を値渡しからin参照渡しに変更

--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -14,10 +14,10 @@ public class Turret : MonoBehaviour
     {
         if (_sight)
         {
-            Rotate(transform, transform, _sight.forward, _speed.x, Vector3.up, Time.fixedDeltaTime);
+            Rotate(transform, transform, _sight.forward, _speed.x, Time.fixedDeltaTime);
             if (_barrel)
             {
-                Pitch(transform, _barrel, _sight.forward, _speed.y, Vector3.right, Time.fixedDeltaTime);
+                Pitch(transform, _barrel, _sight.forward, _speed.y, Time.fixedDeltaTime);
             }
         }
     }
@@ -31,15 +31,15 @@ public class Turret : MonoBehaviour
     /// <param name="speed"></param>
     /// <param name="axis"></param>
     /// <param name="deltaTime"></param>
-    void Rotate(Transform coordinateTransform, Transform selfTransform, Vector3 forward, float speed, Vector2 axis, float deltaTime)
+    void Rotate(in Transform coordinateTransform, Transform selfTransform, in Vector3 forward, in float speed, in float deltaTime)
     {
         Vector3 dir = coordinateTransform.InverseTransformDirection(forward);
-        float side = Vector3.Dot(Vector3.Cross(Vector3.back, axis), dir);
+        float side = Vector3.Dot(Vector3.right, dir);
         float y = Mathf.Atan2(side, dir.z) / Mathf.PI * 180;
-        float lr = y / Mathf.Abs(y);
-        float angle = Mathf.Abs(speed * deltaTime) < Mathf.Abs(y) ? speed * deltaTime * lr : y;
+        float lr = y < 0 ? -1 : 1;
+        float angle = speed * deltaTime < Mathf.Abs(y) ? speed * deltaTime * (y < 0 ? -1 : 1) : y;
 
-        selfTransform.Rotate(axis, angle, Space.Self);
+        selfTransform.Rotate(Vector3.up, angle, Space.Self);
     }
 
     /// <summary>
@@ -51,7 +51,7 @@ public class Turret : MonoBehaviour
     /// <param name="speed"></param>
     /// <param name="axis"></param>
     /// <param name="deltaTime"></param>
-    void Pitch(Transform coordinateTransform, Transform selfTransform, Vector3 forward, float speed, Vector2 axis, float deltaTime)
+    void Pitch(in Transform coordinateTransform, Transform selfTransform, in Vector3 forward, float speed,  in float deltaTime)
     {
         float sightDot = Mathf.Clamp(Vector3.Dot(coordinateTransform.up, forward), -1, 1);
         float selfDot = Mathf.Clamp(Vector3.Dot(coordinateTransform.up, selfTransform.forward), -1, 1);
@@ -60,6 +60,6 @@ public class Turret : MonoBehaviour
         float y = sight - self;
         int up = (selfTransform.up.y < 0 ? -1 : 1);
         float angle = speed * deltaTime < Mathf.Abs(y) ? speed * deltaTime * (y > 0 ? 1 : -1) : y * up;
-        selfTransform.Rotate(axis, angle, Space.Self);
+        selfTransform.Rotate(Vector3.right, angle, Space.Self);
     }
 }
