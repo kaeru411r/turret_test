19d78203a2efaa629af90b1d7bfcbdccf1a4aa8d
kaeru411r <ike269fr@gmail.com>
2022-12-27 18:24:47 +0900

    引数にコメント追加

--- Assets/Turret.cs
+++ Assets/Turret.cs
@@ -13,43 +13,45 @@ public class Turret : MonoBehaviour
             transform.rotation = Rotate(transform, _sight.forward, _speed.x, Time.fixedDeltaTime);
             if (_barrel)
             {
-                _barrel.rotation = Pitch(transform, _barrel, _sight.forward, _speed.y, Time.fixedDeltaTime);
+                _barrel.rotation = Pitch(_barrel, transform.up, _sight.forward, _speed.y, Time.fixedDeltaTime);
             }
         }
     }
 
     /// <summary>
-    /// 
+    /// 旋回を計算する
     /// </summary>
-    /// <param name="transform"></param>
-    /// <param name="forward"></param>
-    /// <param name="speed"></param>
-    /// <param name="deltaTime"></param>
+    /// <param name="transform">動かすオブジェクトのTransform</param>
+    /// <param name="forward">サイトの正面ベクトル</param>
+    /// <param name="speed">旋回速度</param>
+    /// <param name="deltaTime">経過時間</param>
+    /// <returns>回転後のQuaternion</returns>
     Quaternion Rotate(in Transform transform, in Vector3 forward, in float speed, in float deltaTime)
     {
         Vector3 dir = transform.InverseTransformDirection(forward);
-        float y = Mathf.Atan2(dir.x, dir.z) / Mathf.PI * 180;
+        float y = Mathf.Atan2(dir.x, dir.z) * Mathf.Deg2Rad;
         float angle = speed * deltaTime < Mathf.Abs(y) ? speed * deltaTime * (y < 0 ? -1 : 1) : y;
 
         return transform.rotation * Quaternion.AngleAxis(angle, Vector3.up);
     }
 
     /// <summary>
-    /// 
+    /// 砲身の動作を計算する
     /// </summary>
-    /// <param name="coordinateTransform"></param>
-    /// <param name="selfTransform"></param>
-    /// <param name="forward"></param>
-    /// <param name="speed"></param>
-    /// <param name="deltaTime"></param>
-    Quaternion Pitch(in Transform coordinateTransform, in Transform selfTransform, in Vector3 forward, in float speed, in float deltaTime)
+    /// <param name="selfTransform">動かすオブジェクトのTransform</param>
+    /// <param name="baseAxis">基部の回転軸</param>
+    /// <param name="forward">サイトの正面ベクトル</param>
+    /// <param name="speed">旋回速度</param>
+    /// <param name="deltaTime">経過時間</param>
+    /// <returns>回転後のQuaternion</returns>
+    Quaternion Pitch(in Transform selfTransform, in Vector3 baseAxis, in Vector3 forward, in float speed, in float deltaTime)
     {
-        float sightDot = Mathf.Clamp(Vector3.Dot(coordinateTransform.up, forward), -1, 1);
-        float selfDot = Mathf.Clamp(Vector3.Dot(coordinateTransform.up, selfTransform.forward), -1, 1);
+        float sightDot = Mathf.Clamp(Vector3.Dot(baseAxis, forward), -1, 1);
+        float selfDot = Mathf.Clamp(Vector3.Dot(baseAxis, selfTransform.forward), -1, 1);
         float sight = Mathf.Acos(sightDot) * Mathf.Rad2Deg;
         float self = Mathf.Acos(selfDot) * Mathf.Rad2Deg;
         float y = sight - self;
-        int up = (Vector3.Dot(coordinateTransform.up, selfTransform.up) < 0 ? -1 : 1);
+        int up = (Vector3.Dot(baseAxis, selfTransform.up) < 0 ? -1 : 1);
         float angle = speed * deltaTime < Mathf.Abs(y) ? speed * deltaTime * (y > 0 ? 1 : -1) : y * up;
 
         return selfTransform.rotation * Quaternion.AngleAxis(angle, Vector3.right);
