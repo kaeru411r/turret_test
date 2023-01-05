f8506cb4511bfc82f255eec7f95b526b1e29394c
kaeru411r <ike269fr@gmail.com>
2023-01-05 22:27:21 +0900

    InverseTransformDirectionのデモ２

--- Assets/InverseTransformDirectionTest.cs
+++ Assets/InverseTransformDirectionTest.cs
@@ -27,94 +27,55 @@ public class InverseTransformDirectionTest : MonoBehaviour
             Vector3 y = _local.up * inverse.y * _length;
             Vector3 z = _local.forward * inverse.z * _length;
 
-            float xz = (y.magnitude <= x.magnitude && y.magnitude <= z.magnitude) ? 1 : 0;
-            float xy = (z.magnitude < x.magnitude && z.magnitude < y.magnitude) ? 1 : 0;
-            float yz = (x.magnitude < y.magnitude && x.magnitude < z.magnitude) ? 1 : 0;
-
             Gizmos.color = Color.red;
             Gizmos.DrawRay(_local.position, x);
             Gizmos.color *= _fanc;
-            Gizmos.DrawRay(_local.position + y, x * xy);
-            Gizmos.DrawRay(_local.position + z, x * xz);
-            Gizmos.DrawRay(_local.position + y + z, x * yz);
+            Gizmos.DrawRay(_local.position + y, x);
+            Gizmos.DrawRay(_local.position + z, x);
+            Gizmos.DrawRay(_local.position + y + z, x);
 
             Gizmos.color = Color.blue;
             Gizmos.DrawRay(_local.position, z);
             Gizmos.color *= _fanc;
-            Gizmos.DrawRay(_local.position + x, z * xz);
-            Gizmos.DrawRay(_local.position + y, z * yz);
-            Gizmos.DrawRay(_local.position + x + y, z * xy);
+            Gizmos.DrawRay(_local.position + x, z);
+            Gizmos.DrawRay(_local.position + y, z);
+            Gizmos.DrawRay(_local.position + x + y, z);
 
             Gizmos.color = Color.green;
             Gizmos.DrawRay(_local.position, y);
             Gizmos.color *= _fanc;
-            Gizmos.DrawRay(_local.position + x, y * xy);
-            Gizmos.DrawRay(_local.position + z, y * yz);
-            Gizmos.DrawRay(_local.position + x + z, y * xz);
+            Gizmos.DrawRay(_local.position + x, y);
+            Gizmos.DrawRay(_local.position + z, y);
+            Gizmos.DrawRay(_local.position + x + z, y);
 
-            Gizmos.color = Color.magenta;
-            Gizmos.DrawRay(_local.position, (x + z) * xz);
-            Gizmos.color *= _fanc;
-            Gizmos.DrawRay(_local.position + y, (x + z) * xz);
-
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawRay(_local.position, (x + y) * xy);
-            Gizmos.color *= _fanc;
-            Gizmos.DrawRay(_local.position + z, (x + y) * xy);
-
-            Gizmos.color = Color.cyan;
-            Gizmos.DrawRay(_local.position, (y + z) * yz);
-            Gizmos.color *= _fanc;
-            Gizmos.DrawRay(_local.position + x, (y + z) * yz);
+            Gizmos.color = Color.white;
+            Gizmos.DrawRay(_local.position, x + y + z);
 
 
             x = new(_world.forward.x * _length, 0, 0);
             y = new(0, _world.forward.y * _length, 0);
             z = new(0, 0, _world.forward.z * _length);
 
-            xz = (y.magnitude <= x.magnitude && y.magnitude <= z.magnitude) ? 1 : 0;
-            xy = (z.magnitude < x.magnitude && z.magnitude < y.magnitude) ? 1 : 0;
-            yz = (x.magnitude < y.magnitude && x.magnitude < z.magnitude) ? 1 : 0;
-
             Gizmos.color = Color.red;
             Gizmos.DrawRay(_world.position, x);
             Gizmos.color *= _fanc;
-            Gizmos.DrawRay(_world.position + y, x * xy);
-            Gizmos.DrawRay(_world.position + z, x * xz);
-            Gizmos.DrawRay(_world.position + y + z, x * yz);
+            Gizmos.DrawRay(_world.position + y, x);
+            Gizmos.DrawRay(_world.position + z, x);
+            Gizmos.DrawRay(_world.position + y + z, x);
 
             Gizmos.color = Color.blue;
             Gizmos.DrawRay(_world.position, z);
             Gizmos.color *= _fanc;
-            Gizmos.DrawRay(_world.position + x, z * xz);
-            Gizmos.DrawRay(_world.position + y, z * yz);
-            Gizmos.DrawRay(_world.position + x + y, z * xy);
+            Gizmos.DrawRay(_world.position + x, z);
+            Gizmos.DrawRay(_world.position + y, z);
+            Gizmos.DrawRay(_world.position + x + y, z);
 
             Gizmos.color = Color.green;
             Gizmos.DrawRay(_world.position, y);
             Gizmos.color *= _fanc;
-            Gizmos.DrawRay(_world.position + x, y * xy);
-            Gizmos.DrawRay(_world.position + z, y * yz);
-            Gizmos.DrawRay(_world.position + x + z, y * xz);
-
-            Gizmos.color = Color.magenta;
-            Gizmos.DrawRay(_world.position, (x + z) * xz);
-            Gizmos.color *= _fanc;
-            Gizmos.DrawRay(_world.position + y, (x + z) * xz);
-
-            Gizmos.color = Color.yellow;
-            Gizmos.DrawRay(_world.position, (x + y) * xy);
-            Gizmos.color *= _fanc;
-            Gizmos.DrawRay(_world.position + z, (x + y) * xy);
-
-            Gizmos.color = Color.cyan;
-            Gizmos.DrawRay(_world.position, (y + z) * yz);
-            Gizmos.color *= _fanc;
-            Gizmos.DrawRay(_world.position + x, (y + z) * yz);
-
-
-            Gizmos.color = Color.white;
-            Gizmos.DrawRay(_local.position, x + y + z);
+            Gizmos.DrawRay(_world.position + x, y);
+            Gizmos.DrawRay(_world.position + z, y);
+            Gizmos.DrawRay(_world.position + x + z, y);
         }
     }
 
